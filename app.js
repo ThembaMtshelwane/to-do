ec483c72c98ddb3c7ca33f02d554bf0f98dceeb8
Themba Mtshelwane <Thembamm3@gmail.com>
2023-06-12 16:45:02 +0200

    Adding pages and server

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const PORT = 3000
+
+// express app
+const app = express()
+
+// register view engine
+app.set('view engine', 'ejs')
+
+// listen for requests
+app.listen(PORT)
+
+//Home Page
+app.get('/', (req,res)=>{
+    res.render('index')
+})
+
+//  Dashboard Page
+app.get('/dashbaord', (req,res)=>{
+    res.render('dashbaord')
+})
+
+//About Page
+app.get('/about', (req,res)=>{
+    res.render('about')
+})
+
+//Create Task
+app.get('/tasks/create', (req,res)=>{
+    res.render('create')
+})
+
+// Page not found
+app.use((req,res)=>{
+    res.status(404).render('404')
+})
