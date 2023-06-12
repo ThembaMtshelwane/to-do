67876930eb2cf95de1fbed81c388bdebdacba11a
Themba Mtshelwane <Thembamm3@gmail.com>
2023-06-12 21:08:45 +0200

    connect database

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const express = require('express')
+const mongoose =  require('mongoose')
+
 const PORT = 3000
 
 // express app
 const app = express()
 
+// connect to mongoDB
+const dbURI = 'mongodb+srv://thembamm3:A4MJL4wQr3pGxCZD@cluster0.ughmouw.mongodb.net/todos?retryWrites=true&w=majority'
+mongoose.connect(dbURI)
+  .then(()=>{
+    // listen for requests
+    app.listen(PORT)
+  })
+  .catch((err)=>{console.log(err)
+})
+
 // register view engine
 app.set('view engine', 'ejs')
-
-// listen for requests
-app.listen(PORT)
 
 // middleware and static files
 app.use(express.static('public'))
